ff6c93db3ab27db09387317495f8d83ce279fea0
azu <azuciao@gmail.com>
2013-07-03 22:38:39 +0900

    use xcodebild

---
+++ script/run-test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+xcodebuild \
+	-workspace NSDate-Escort.xcworkspace \
+	-sdk iphonesimulator \
+	-scheme Tests \
+	-configuration Debug \
+	clean build \
+	ONLY_ACTIVE_ARCH=NO \
+	TEST_AFTER_BUILD=YES \
+	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+	GCC_GENERATE_TEST_COVERAGE_FILES=YES
