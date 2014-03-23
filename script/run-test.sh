bb355eb7ec938e6fa3ac8e290f4a823995cbd83b
azu <azuciao@gmail.com>
2014-03-23 12:22:06 +0900

    fix(travis): install dependency before install

--- script/run-test.sh
+++ script/run-test.sh
@@ -5,6 +5,7 @@ xcodebuild clean test\
 	-workspace NSDate-Escort.xcworkspace \
 	-scheme Test \
 	-configuration Debug \
+    OBJROOT=build \
 	ONLY_ACTIVE_ARCH=NO \
 	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
 	GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
