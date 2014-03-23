98c96d43efb510f49a5224d7e59492bb3342f12e
azu <azuciao@gmail.com>
2014-03-23 12:19:04 +0900

    fix(run-test): use xcodebuild and xcpretty.

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-xctool test \
+xcodebuild clean test\
+    -sdk iphonesimulator \
 	-workspace NSDate-Escort.xcworkspace \
-	-sdk iphonesimulator \
-	-scheme Tests \
+	-scheme Test \
 	-configuration Debug \
 	ONLY_ACTIVE_ARCH=NO \
 	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
-	GCC_GENERATE_TEST_COVERAGE_FILES=YES
+	GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
