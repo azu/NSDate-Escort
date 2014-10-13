cf28b710c27bffb483b910e25cd774707585b39b
azu <azuciao@gmail.com>
2014-10-13 20:47:30 +0900

    update simulator

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 xcodebuild test -workspace NSDate-Escort.xcworkspace -scheme 'Test'\
- -destination 'platform=iOS Simulator,name=iPhone Retina (4-inch)' | xcpretty -c
+ -destination 'platform=iOS Simulator,name=iPhone 6' | xcpretty -c
