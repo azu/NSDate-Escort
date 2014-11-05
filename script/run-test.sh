f2d9178a68ca3387016a5a6d88edda7e86198a97
azu <azuciao@gmail.com>
2014-11-06 00:13:08 +0900

    update test script

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-xcodebuild test -workspace NSDate-Escort.xcworkspace -scheme 'Test'\
- -destination 'platform=iOS Simulator,name=iPhone 6' | xcpretty -c
+xcodebuild test -workspace NSDate-Escort.xcworkspace \
+-scheme 'Test' \
+-sdk iphonesimulator \
+-destination 'platform=iOS Simulator,OS=8.0,name=iPhone 6' | xcpretty -c && exit ${PIPESTATUS[0]}
