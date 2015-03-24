d96f9daaecf3484bf6ad8b0144071fa17bb03d4d
azu <azuciao@gmail.com>
2015-03-24 13:46:23 +0900

    remove xcpretty

--- script/run-test.sh
+++ script/run-test.sh
@@ -5,4 +5,4 @@ xcodebuild test -workspace NSDate-Escort.xcworkspace \
 -sdk iphonesimulator \
 -destination 'platform=iOS Simulator,OS=8.1,name=iPhone 6' \
 GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
-GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c && exit ${PIPESTATUS[0]}
+GCC_GENERATE_TEST_COVERAGE_FILES=YES
