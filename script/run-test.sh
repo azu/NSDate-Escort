c70eeb957c010293c1eeaf3e96202bede42100eb
akuraru <akuraru@gmail.com>
2018-10-17 23:32:35 +0900

    update travis ci 2

--- script/run-test.sh
+++ script/run-test.sh
@@ -3,6 +3,5 @@
 xcodebuild test -workspace NSDate-Escort.xcworkspace \
 -scheme 'Test' \
 -sdk iphonesimulator \
--destination 'platform=iOS Simulator,OS=11.4,name=iPhone X' \
-GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+-destination 'platform=iOS Simulator,OS=12.0,name=iPhone XS' \
 GCC_GENERATE_TEST_COVERAGE_FILES=YES
