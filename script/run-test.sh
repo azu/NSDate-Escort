2100a2c7c81cd0df345dd2c2284931849473501c
akuraru <akuraru@gmail.com>
2019-05-03 19:05:50 +0900

    update travis xcode 10.2

--- script/run-test.sh
+++ script/run-test.sh
@@ -3,5 +3,5 @@
 xcodebuild test -workspace NSDate-Escort.xcworkspace \
 -scheme 'Test' \
 -sdk iphonesimulator \
--destination 'platform=iOS Simulator,OS=12.0,name=iPhone XS' \
+-destination 'platform=iOS Simulator,OS=12.2,name=iPhone XS' \
 GCC_GENERATE_TEST_COVERAGE_FILES=YES
